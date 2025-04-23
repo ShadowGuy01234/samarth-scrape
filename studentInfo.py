901fb7a0198920287f55994c5f05ed84cc194237
Anurag Banerjee <121357713+ShadowGuy01234@users.noreply.github.com>
2025-04-23 12:31:19 +0530

    implemented login to samarth

--- studentInfo.py
+++ studentInfo.py
@@ -1,15 +1,34 @@
 from bs4 import BeautifulSoup
 import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+username = os.getenv("username")
+password = os.getenv("password")
 
 URL = "https://mmmut.samarth.edu.in/index.php/examstudent/data-correction/view?id="
 
-response = requests.get(URL)
-soup = BeautifulSoup(response.text, 'html.parser')
+# Login to samarth
+session = requests.Session()
+login_url = "https://mmmut.samarth.edu.in/index.php/site/login"
 
-metaData = soup.find('meta', {'name' : 'csrf-token'})
-StudentID = metaData['content'] if metaData else None
+login_page = session.get(login_url)
 
-newURL = f"https://mmmut.samarth.edu.in/index.php/examstudent/data-correction/view?id={StudentID}"
+new_soup = BeautifulSoup(login_page.content, 'html.parser')
+csrf_token = new_soup.find('input', {'name': '_csrf'}).get('value')
 
-# print(StudentID)
-# print(soup)
+login_data = {
+            'LoginForm[username]': username,
+            'LoginForm[password]': password,
+            '_csrf': csrf_token
+        }
+
+headers = {'User-Agent': 'Mozilla/5.0'}
+
+login_response = session.post(login_url, data=login_data, headers=headers)
+print(f"Status code: {login_response.status_code}")
+
+
+
